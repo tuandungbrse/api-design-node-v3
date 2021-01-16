c57e41dbe0b5186e231352bcb72d6788aabb0382
Truong Nguyen Tuan Dung <tuandungbrse@gmail.com>
2021-01-16 14:51:48 +0700

    finish solution for exercise 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,15 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => res.send({ message: 'GET ALL' }))
+  .post((req, res) => res.send({ message: 'POST ONE' }))
+
+router
+  .route('/:id')
+  .get((req, res) => res.send({ message: 'GET ONE' }))
+  .put((req, res) => res.send({ message: 'UPDATE ONE' }))
+  .delete((req, res) => res.send({ message: 'DELETE ONE' }))
+
 export default router
